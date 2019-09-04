2cfa87f59ba7721ce82501e12574f160fae32b4b
jakasco <kasperi.mutku@elisanet.fi>
2019-09-04 13:28:27 +0300

    Test commit

---
+++ Models/Authorization.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConnectingThings.Models
+{
+    public class Authorization
+    {
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
